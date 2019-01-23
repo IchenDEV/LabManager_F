41babae570b95d9efa627d793a5cbe0a3ffe4f7c
陈力 <cl137046952@outlook.com>
2019-01-23 17:30:35 +0800

    make qrScanner better

--- src/components/QRScanner/QRScanner.vue
+++ src/components/QRScanner/QRScanner.vue
@@ -1,160 +1,166 @@
 <template>
   <div>
-    <div id="result" v-show="showRes"></div>
-    <video id="v" autoplay></video>
+    <video :class="{blur:isBlur}" id="v" autoplay ></video>
     <canvas id="qr-canvas" width="600" height="600" style="display:none;"></canvas>
+    <div id="result" v-show="showRes"></div>
   </div>
 </template>
 <script>
 /* eslint-disable */
-var gCtx = null;
-var gCanvas = null;
-var c = 0;
-var stype = 0;
-var gUM = false;
-var webkit = false;
-var moz = false;
-var v = null;
-var that=null;
-function initCanvas(w, h) {
-  gCanvas = document.getElementById("qr-canvas");
-  gCanvas.style.width = w + "px";
-  gCanvas.style.height = h + "px";
-  gCanvas.width = w;
-  gCanvas.height = h;
-  gCtx = gCanvas.getContext("2d");
-  gCtx.clearRect(0, 0, w, h);
-}
-
-function captureToCanvas() {
-  if (stype != 1) return;
-  if (gUM) {
-    try {
-      gCtx.drawImage(v, 0, 0);
-      try {
-        qrcode.decode();
-      } catch (e) {
-        console.log(e);
-        setTimeout(captureToCanvas, 500);
-      }
-    } catch (e) {
-      console.log(e);
-      setTimeout(captureToCanvas, 500);
-    }
-  }
-}
-
-function htmlEntities(str) {
-  return String(str)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
-}
-
-function read(a) {
-  var html = "";
-  if (a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
-    html += "<a target='_blank' href='" + a + "'>" + a + "</a><br>";
-  html = htmlEntities(a);
-  that.onrecived(html);
-  document.getElementById("result").innerHTML = html;
-}
-
-function isCanvasSupported() {
-  var elem = document.createElement("canvas");
-  return !!(elem.getContext && elem.getContext("2d"));
-}
-function success(stream) {
-  v.srcObject = stream;
-  v.play();
-  gUM = true;
-  setTimeout(captureToCanvas, 500);
-}
-
-function error(error) {
-  gUM = false;
-  return;
-}
-
-function setwebcam() {
-  var options = true;
-  if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
-    try {
-      navigator.mediaDevices.enumerateDevices().then(function(devices) {
-        devices.forEach(function(device) {
-          if (device.kind === "videoinput") {
-            if (device.label.toLowerCase().search("back") > -1)
-              options = {
-                deviceId: { exact: device.deviceId },
-                facingMode: "environment"
-              };
-          }
-          console.log(
-            device.kind + ": " + device.label + " id = " + device.deviceId
-          );
-        });
-        setwebcam2(options);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  } else {
-    console.log("no navigator.mediaDevices.enumerateDevices");
-    setwebcam2(options);
-  }
-}
-
-function setwebcam2(options) {
-  console.log(options);
-  document.getElementById("result").innerHTML = "- scanning -";
-  if (stype === 1) {
-    setTimeout(captureToCanvas, 500);
-    return;
-  }
-  var n = navigator;
-
-  v = document.getElementById("v");
-
-  if (n.mediaDevices.getUserMedia) {
-    n.mediaDevices
-      .getUserMedia({ video: options, audio: false })
-      .then(function(stream) {
-        success(stream);
-      })
-      .catch(function(error) {
-        error(error);
-      });
-  } else if (n.getUserMedia) {
-    webkit = true;
-    n.getUserMedia({ video: options, audio: false }, success, error);
-  } else if (n.webkitGetUserMedia) {
-    webkit = true;
-    n.webkitGetUserMedia({ video: options, audio: false }, success, error);
-  }
-  stype = 1;
-  setTimeout(captureToCanvas, 500);
-}
+var that=null
 export default {
   name: "QRScanner",
   props: { showRes: { default: false } },
+  data() {
+    return {
+      gCtx: null,
+      gCanvas: null,
+      c: 0,
+      stype: 0,
+      gUM: false,
+      webkit: false,
+      v: null,
+      isBlur:false
+    };
+  },
   methods: {
     load() {
-      if (isCanvasSupported() && window.File && window.FileReader) {
-        initCanvas(800, 600);
-        qrcode.callback = read;
-        setwebcam();
+      if (this.isCanvasSupported() && window.File && window.FileReader) {
+        this.initCanvas(800, 600);
+        qrcode.callback = this.read;
+        this.setwebcam();
       }
     },
-    onrecived(info){
-      //alert(a)
-      this.$emit('recived',info)
+     setwebcam2(options) {
+      console.log(options);
+      document.getElementById("result").innerHTML = "- scanning -";
+      if (this.stype === 1) {
+        setTimeout(this.captureToCanvas, 500);
+        return;
+      }
+      var n = navigator;
+
+      this.v = document.getElementById("v")
+      
+      if (n.mediaDevices.getUserMedia) {
+        n.mediaDevices
+          .getUserMedia({ video: options, audio: false })
+          .then(function(stream) {
+            that.v.srcObject = stream;
+            that.v.play();
+            that.gUM = true;
+            setTimeout(that.captureToCanvas, 500);
+          })
+          .catch(function(error) {
+            that.gUM = false;
+          });
+      } else if (n.getUserMedia) {
+        this.webkit = true;
+        n.getUserMedia({ video: options, audio: false }, success, error);
+      } else if (n.webkitGetUserMedia) {
+        this.webkit = true;
+        n.webkitGetUserMedia({ video: options, audio: false }, success, error);
+      }
+      this.stype = 1;
+      setTimeout(this.captureToCanvas, 500);
+    },
+    setwebcam() {
+      
+      var options = true;
+      if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
+        try {
+          navigator.mediaDevices.enumerateDevices().then(function(devices) {
+            devices.forEach(function(device) {
+              if (device.kind === "videoinput") {
+                if (device.label.toLowerCase().search("back") > -1)
+                  options = {
+                    deviceId: { exact: device.deviceId },
+                    facingMode: "environment"
+                  };
+              }
+              console.log(
+                device.kind + ": " + device.label + " id = " + device.deviceId
+              );
+            });
+            that.setwebcam2(options);
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        console.log("no navigator.mediaDevices.enumerateDevices");
+        that.setwebcam2(options);
+      }
+    },
+   
+    read(a) {
+      var html = "";
+      if (a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
+        html += "<a target='_blank' href='" + a + "'>" + a + "</a><br>";
+      html = this.htmlEntities(a);
+      this.onrecived(html);
+      document.getElementById("result").innerHTML = html;
+    },
+
+    isCanvasSupported() {
+      var elem = document.createElement("canvas");
+      return !!(elem.getContext && elem.getContext("2d"));
+    },
+    initCanvas(w, h) {
+      this.gCanvas = document.getElementById("qr-canvas");
+      this.gCanvas.style.width = w + "px";
+      this.gCanvas.style.height = h + "px";
+      this.gCanvas.width = w;
+      this.gCanvas.height = h;
+      this.gCtx = this.gCanvas.getContext("2d");
+      this.gCtx.clearRect(0, 0, w, h);
+    },
+
+    htmlEntities(str) {
+      return String(str)
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;");
+    },
+    captureToCanvas() {
+      if (this.stype != 1) return;
+      if (this.gUM) {
+        try {
+          this.gCtx.drawImage(v, 0, 0);
+          try {
+            qrcode.decode();
+          } catch (e) {
+            console.log(e);
+            setTimeout(this.captureToCanvas, 500);
+          }
+        } catch (e) {
+          console.log(e);
+          setTimeout(this.captureToCanvas, 500);
+        }
+      }
+    },
+    onrecived(info) {
+      this.isBlur=true;
+      this.v.pause();
+      this.$emit("recived", info);
     }
   },
   mounted() {
     that=this
     this.load()
-  }
+  },
+  
 };
 </script>
+<style>
+.blur {	
+    -webkit-filter: blur(10px); /* Chrome, Opera */
+       -moz-filter: blur(10px);
+        -ms-filter: blur(10px);    
+            filter: blur(10px);
+    
+    filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius=10, MakeShadow=false); /* IE6~IE9 */
+}
+</style>
 
