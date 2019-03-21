10daf494aa4fb8400b08e44e07d9fc92d4d233ec
朱庆鹏 <1226358753@qq.com>
2019-03-21 17:37:54 +0800

    页脚自适应

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,18 +1,35 @@
 <template>
-  <footer class="footer">
+  <Footer class="layout-footer-center" :style="{minHeight:minHeight + 'px'}">
+    <!--页脚蓝色框和文字是否可以精简-->
     <ui-alert :dismissible="false">{{$t('message.footerAlert')}}</ui-alert>
+    <div align="center" margin-top:100px>
     <h2>{{$t('message.productName')}}</h2>
     <p>{{$t('message.devBy')}}</p>
     <p>Beta</p>
     <a href="https://www.perficient.com">{{$t("message.links")}}</a>
-  </footer>
+    </div>
+  </Footer>
 </template>
+<!--计算了窗口高度来使页面底部自适应-->
 <script>
-export default {}
+export default {
+  data(){
+    return{
+      minHeight:0
+    }
+  },
+  mounted(){
+    this.minHeight = document.documentElement.clientHeight - 350
+    var that = this
+    window.onresize = function (){
+      that.minHeight = document.documentElement.clientHeight -350
+    }
+  }
+}
 </script>
 <style>
-.footer {
-  margin-top: 100px;
+.layout-footer-center {
+  margin-top: 200px; 
   background: #ffffff;
   font-size: 0.9rem;
   padding: 1.5rem;
@@ -20,5 +37,6 @@ export default {}
   z-index: -1;
   border-radius: .5rem .5rem 0 0;
   -webkit-transform: translateZ(10);-moz-transform: translateZ(10);-ms-transform: translateZ(10);transform: translateZ(10);
+  text-align: center;
 }
 </style>
